6c93f0df62a8085a280fc475d3ddec7979e71839
Alessandro Ronchi <aleron75@gmail.com>
2014-07-13 00:48:04 +0200

    Add empty useless initial shell script

---
+++ src/shell/golive.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'abstract.php';
+
+class Mage_Shell_Webgriffe_Golive extends Mage_Shell_Abstract
+{
+    public function run()
+    {
+
+    }
+}
+
+$shell = new Mage_Shell_Webgriffe_Golive();
+$shell->run();
