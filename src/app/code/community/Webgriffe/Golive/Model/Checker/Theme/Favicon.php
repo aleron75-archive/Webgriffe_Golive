5d597433e0e0fc385318ecbdedc88d285d2f982b
Alessandro Ronchi <aleron75@gmail.com>
2014-07-14 01:08:25 +0200

    Add Favicon Checker

---
+++ src/app/code/community/Webgriffe/Golive/Model/Checker/Theme/Favicon.php
@@ -0,0 +1,23 @@
+<?php
+class Webgriffe_Golive_Model_Checker_Theme_Favicon
+    extends Webgriffe_Golive_Model_Checker_Abstract
+{
+    const MD5SUM_DEFAULT_FAVICON = "88733ee53676a47fc354a61c32516e82";
+
+    public function check($parameters = array())
+    {
+        if (!isset($parameters['theme'])) {
+            Mage::throwException(sprintf("Parameter 'theme' is missing."));
+        }
+
+        $theme = $parameters['theme'];
+
+        $filename = Mage::getBaseDir('skin').DS.'frontend'.DS.$theme.DS.'favicon.ico';
+        $currentFaviconMd5 = md5_file($filename);
+        if ($currentFaviconMd5 == self::MD5SUM_DEFAULT_FAVICON) {
+            return $this->getDefaultSeverity();
+        }
+
+        return self::SEVERITY_NONE;
+    }
+}
