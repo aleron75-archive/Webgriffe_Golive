f0e3b027dc72d3f191e867368296c6e9b48c32bf
Alessandro Ronchi <aleron75@gmail.com>
2014-07-16 23:11:46 +0200

    First embryonic shell script to use checkers

--- src/shell/golive.php
+++ src/shell/golive.php
@@ -5,8 +5,40 @@ class Mage_Shell_Webgriffe_Golive extends Mage_Shell_Abstract
 {
     public function run()
     {
+        $domain = $this->getArg('domain');
 
+        if (empty($domain))
+        {
+            print $this->usageHelp();
+            exit(1);
+        }
+
+        $parameters =     array(
+            'domain'    => $domain,
+        );
+
+        $golive = new Webgriffe_Golive_Model_Core();
+        $result = $golive->check($parameters);
+
+        Zend_Debug::dump($result, 'Result');
     }
+
+    /**
+     * Retrieve Usage Help Message
+     *
+     */
+    public function usageHelp()
+    {
+        return <<<USAGE
+Usage:  php -f golive.php -- [options]
+        php -f golive.php --domain www.yourdomain.com
+
+  --domain <domain> The domain against which this site configuration will be tested
+  help              This help
+
+USAGE;
+    }
+
 }
 
 $shell = new Mage_Shell_Webgriffe_Golive();
