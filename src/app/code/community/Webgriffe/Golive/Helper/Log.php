a0995a7cb4c3a77f272c8cf0e7fbc077c8517ec5
Alessandro Ronchi <aleron75@gmail.com>
2014-07-16 22:40:01 +0200

    Added error function to Logger: duplicated code to refactor

--- src/app/code/community/Webgriffe/Golive/Helper/Log.php
+++ src/app/code/community/Webgriffe/Golive/Helper/Log.php
@@ -29,4 +29,21 @@ class Webgriffe_Golive_Helper_Log extends Mage_Core_Helper_Abstract
         );
         Mage::log($msg, Zend_Log::INFO, self::LOG_FILENAME, $this->_forceLog);
     }
+
+    public function error()
+    {
+        $args = func_get_args();
+        $formattedMsg = call_user_func_array('sprintf', $args);
+        $backtrace = debug_backtrace();
+        if (count($backtrace) > 1) {
+            array_shift($backtrace);
+        }
+        $msg = sprintf(
+            '[%s->%s()] %s',
+            $backtrace[0]['class'],
+            $backtrace[0]['function'],
+            $formattedMsg
+        );
+        Mage::log($msg, Zend_Log::ERR, self::LOG_FILENAME, $this->_forceLog);
+    }
 }
