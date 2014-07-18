5999981c03448bf6e0a70d9c0a9f1ff2c9dc2cc5
Alessandro Ronchi <aleron75@gmail.com>
2014-07-18 22:21:16 +0200

    Refactoring + remove of useless logging

--- src/app/code/community/Webgriffe/Golive/Model/Checker/Config/UnsecureBaseUrl.php
+++ src/app/code/community/Webgriffe/Golive/Model/Checker/Config/UnsecureBaseUrl.php
@@ -29,13 +29,12 @@ class Webgriffe_Golive_Model_Checker_Config_UnsecureBaseUrl
 
     public function validateParameters(&$parameters)
     {
-        $this->_logger->info("Parameters before validate(): %s", print_r($parameters, 1));
         parent::validateParameters($parameters);
 
         if (!isset($parameters['domain'])) {
-            $this->_logger->error("Parameter 'domain' is missing.");
-            Mage::throwException(sprintf("Parameter 'domain' is missing."));
+            $msg = "Parameter 'domain' is missing.";
+            $this->_logger->error($msg);
+            Mage::throwException($msg);
         }
-        $this->_logger->info("Parameters after validate(): %s", print_r($parameters, 1));
     }
 }
