dab16648ed4afcdd654c35eab87df7fcecf3b072
Alessandro Ronchi <aleron75@gmail.com>
2014-08-15 10:41:19 +0200

    Refines functions to print tables, still not used

--- src/app/code/community/Webgriffe/Golive/Helper/Output.php
+++ src/app/code/community/Webgriffe/Golive/Helper/Output.php
@@ -18,34 +18,49 @@ class Webgriffe_Golive_Helper_Output extends Mage_Core_Helper_Abstract
     /**
      * Usage
      *
-     * printTr(array('col1', 'col2', 'col3'), array('10', '-10', '5'));
+     * printTr(array('col1', 'col2', 'col3'), array('10', '-10', '5'), array(31, 33, 36));
+     * printTr(array('col1', 'col2', 'col3'), array('10', '-10', '5'), 33);
      *
      * @param array $columns
      */
     public function printTr($columnStrings, $columnStyles, $columnColors = null)
     {
-        $colcount = count($columnStrings);
-        if ($colcount != count($columnStyles)) {
-            throw new Exception("Number of columns must match");
+        $numColumns = count($columnStrings);
+        if ($numColumns != count($columnStyles)) {
+            Mage::throwException("Number of columns must match");
         }
 
         if (is_string($columnColors) || is_int($columnColors)) {
-            $columnColors = array_fill(0, $colcount, $columnColors);
+            $columnColors = array_fill(0, $numColumns, $columnColors);
         }
 
         $mask = '';
-        for ($i = 0; $i < $colcount; $i ++)
+        $cellRows = array();
+        $maxCellRows = 0;
+
+        for ($i = 0; $i < $numColumns; $i ++)
         {
             if (!is_null($columnColors) && isset($columnColors[$i])) {
-                $mask .= "| \033[".$columnColors[$i]."m%".$columnStyles[$i]."s\033[0m ";
+                $mask .= "| \033[" . $columnColors[$i] . "m%" . $columnStyles[$i] . "s\033[0m ";
             } else {
                 $mask .= '| %' . $columnStyles[$i] . 's ';
             }
+            $width = abs(intval($columnStyles[$i]));
+            $lines = $this->getLines($columnStrings[$i], $width);
+            $cellRows[] = $lines;
+            $maxCellRows = max($maxCellRows, count($lines));
         }
         $mask .= '|';
 
-        $str = vsprintf($mask, $columnStrings);
-        $this->printLine($str);
+        for ($rowIndex = 0; $rowIndex < $maxCellRows; $rowIndex ++)
+        {
+            $row = array();
+            for ($colIndex = 0; $colIndex < $numColumns; $colIndex ++)
+            {
+                $row[] = isset($cellRows[$colIndex][$rowIndex]) ? $cellRows[$colIndex][$rowIndex] : '';
+            }
+            $this->printLine(vsprintf($mask, $row));
+        }
     }
 
     public function printTl($columnStyles)
