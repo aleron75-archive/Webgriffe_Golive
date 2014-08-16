7ceb70a4ccc42ab2e4c19b9117d3336ed21bf958
Alessandro Ronchi <aleron75@gmail.com>
2014-08-16 07:35:20 +0200

    Does several refactorings to shorten methods lenght

--- src/shell/golive.php
+++ src/shell/golive.php
@@ -4,6 +4,16 @@ require_once 'abstract.php';
 class Mage_Shell_Webgriffe_Golive extends Mage_Shell_Abstract
 {
     /**
+     * @var Webgriffe_Golive_Model_Core The Go Live Core Object
+     */
+    private $_core = null;
+
+    /**
+     * @var Webgriffe_Golive_Helper_Output The Output Object
+     */
+    private $_out = null;
+    
+    /**
      * @var bool Indicates whether to use colors or not.
      */
     private $_useColors = true;
@@ -15,101 +25,16 @@ class Mage_Shell_Webgriffe_Golive extends Mage_Shell_Abstract
 
     public function run()
     {
-        $out = Mage::helper('webgriffe_golive/output');
+        $this->_out->printLine(sprintf("Webgriffe Go Live %s", Mage::helper('webgriffe_golive')->getVersion()));
 
-        $golive = new Webgriffe_Golive_Model_Core();
-
-        $out->printLine(sprintf("Webgriffe Go Live %s", Mage::helper('webgriffe_golive')->getVersion()));
-
-        if (count($this->_explainIds))
+        if ($this->_shouldExplain())
         {
-            $out->printLine();
-
-            foreach ($this->_explainIds as $id)
-            {
-                if ($checker = $golive->getCheckerById($id))
-                {
-                    $name = $checker->getName();
-                    $description = $checker->getDescription();
-                    if (empty($description)) {
-                        $description = '>>> Missing description for this checker';
-                    }
-                    $styles = array('-68');
-                    $out->printTl($styles);
-                    $styles = array('3', '-62');
-                    $out->printTr(array($id, $name), $styles, $this->_useColors ? $out::STYLE_BOLD : '');
-                    $styles = array('-68');
-                    $out->printTl($styles);
-                    $out->printTr(array($description), $styles);
-                }
-            }
-            $out->printTl($styles);
-            $out->printLine();
-
+            $this->_outputExplanation();
             exit(0);
         }
 
-        $out->printLine(sprintf("Active Checkers found: %d", $golive->getCheckersCount()));
-        $out->printLine("Checking current Magento installation... ", 0);
-
-        $parameters =     array(
-            'domain'    => $this->getArg('domain'),
-        );
-
-        $result = $golive->check($parameters);
-
-        $out->printLine("done!", 2);
-
-        $severityCount = array(
-            Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_SKIP => 0,
-            Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_NONE => 0,
-            Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_WARNING => 0,
-            Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_ERROR => 0
-        );
-        $maxlen = 0;
-        foreach ($result as $key => $val) {
-            $severityCount[$val] ++;
-            $checker = $golive->getChecker($key);
-            $maxlen = max($maxlen, strlen($checker->getName()));
-        }
-
-
-        $styles = array('3', '-52', '-7');
-        $out->printTl($styles);
-        $out->printTr(array('ID', 'Checked', 'Result'), $styles, $out::STYLE_BOLD);
-        $out->printTl($styles);
-
-        $id = 1;
-        foreach ($result as $code => $res) {
-            $checker = $golive->getChecker($code);
-            if ($this->_useColors) {
-                switch ($res) {
-                    case Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_ERROR:
-                        $color = $out::COLOR_RED;
-                        break;
-                    case Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_WARNING:
-                        $color = $out::COLOR_YELLOW;
-                        break;
-                    case Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_SKIP:
-                        $color = $out::COLOR_CYAN;
-                        break;
-                    default:
-                        $color = $out::COLOR_DEFAULT;
-                }
-            }
-            $columns = array($id ++, $checker->getName(), $res);
-            $out->printTr($columns, $styles, $color);
-        }
-
-        $styles = array('58', '-7');
-        $out->printTl($styles);
-        $out->printTr(array('Errors', $severityCount[Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_ERROR]), $styles);
-        $out->printTr(array('Warnings', $severityCount[Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_WARNING]), $styles);
-        $out->printTr(array('Passed', $severityCount[Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_NONE]), $styles);
-        $out->printTr(array('Skipped', $severityCount[Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_SKIP]), $styles);
-        $out->printTl($styles);
-        $out->printLine();
-        exit($severityCount[Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_ERROR]);
+        $exitCode = $this->_outputCheckAndGetExitCode();
+        exit($exitCode);
     }
 
     /**
@@ -121,6 +46,10 @@ class Mage_Shell_Webgriffe_Golive extends Mage_Shell_Abstract
     {
         parent::_construct();
 
+        $this->_core = new Webgriffe_Golive_Model_Core();
+        
+        $this->_out = Mage::helper('webgriffe_golive/output');
+        
         if (isset($this->_args['nocolors']))
         {
             $this->_useColors = false;
@@ -167,6 +96,157 @@ Usage:  php -f golive.php --[option] --[option <option_value>]
   -h, --help        This help
 
 USAGE;
+    }
+
+    /**
+     * Contains logic which deterimnes whether an explain is needed
+     *
+     * @return int
+     */
+    protected function _shouldExplain()
+    {
+        return count($this->_explainIds);
+    }
+
+    /**
+     * Output the explanation for all the requested Checker IDs
+     */
+    protected function _outputExplanation()
+    {
+        $this->_out->printLine();
+
+        foreach ($this->_explainIds as $id) {
+            if ($checker = $this->_core->getCheckerById($id)) {
+                $name = $checker->getName();
+                $description = $checker->getDescription();
+                if (empty($description)) {
+                    $description = '>>> Missing description for this checker';
+                }
+                $styles = array('-68');
+                $this->_out->printTl($styles);
+                $styles = array('3', '-62');
+                $this->_out->printTr(
+                    array($id, $name),
+                    $styles,
+                    $this->_useColors ? Webgriffe_Golive_Helper_Output::STYLE_BOLD : null
+                );
+                $styles = array('-68');
+                $this->_out->printTl($styles);
+                $this->_out->printTr(array($description), $styles);
+            }
+        }
+        $this->_out->printTl($styles);
+        $this->_out->printLine();
+    }
+
+    /**
+     * @return array
+     */
+    protected function _outputCheckAndGetExitCode()
+    {
+        $this->_out->printLine(
+            sprintf(
+                "Active Checkers found: %d",
+                $this->_core->getCheckersCount()
+            )
+        );
+        $this->_out->printLine("Checking current Magento installation... ", 0);
+
+        $parameters = array(
+            'domain' => $this->getArg('domain'),
+        );
+
+        $result = $this->_core->check($parameters);
+
+        $this->_out->printLine("done!", 2);
+
+        $severityCount = array(
+            Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_SKIP => 0,
+            Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_NONE => 0,
+            Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_WARNING => 0,
+            Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_ERROR => 0
+        );
+        foreach ($result as $key => $val) {
+            $severityCount[$val]++;
+        }
+
+        $styles = array('3', '-52', '-7');
+        $this->_out->printTl($styles);
+        $this->_out->printTr(
+            array('ID', 'Checked', 'Result'),
+            $styles,
+            $this->_useColors ? Webgriffe_Golive_Helper_Output::STYLE_BOLD : null
+        );
+        $this->_out->printTl($styles);
+
+        $id = 1;
+        foreach ($result as $code => $res) {
+            $checker = $this->_core->getChecker($code);
+            $textStyle = null;
+            if ($this->_useColors) {
+                $textStyle = $this->_getTextStyleForResult($res);
+            }
+            $columns = array($id++, $checker->getName(), $res);
+            $this->_out->printTr($columns, $styles, $textStyle);
+        }
+
+        $styles = array('58', '-7');
+        $this->_out->printTl($styles);
+        $this->_out->printTr(
+            array(
+                'Errors',
+                $severityCount[Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_ERROR]
+            ),
+            $styles
+        );
+        $this->_out->printTr(
+            array(
+                'Warnings',
+                $severityCount[Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_WARNING]
+            ),
+            $styles
+        );
+        $this->_out->printTr(
+            array(
+                'Passed',
+                $severityCount[Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_NONE]
+            ),
+            $styles
+        );
+        $this->_out->printTr(
+            array(
+                'Skipped',
+                $severityCount[Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_SKIP]
+            ),
+            $styles
+        );
+        $this->_out->printTl($styles);
+        $this->_out->printLine();
+
+        return $severityCount[Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_ERROR];
+    }
+
+    /**
+     * @param $res
+     * @return int
+     */
+    protected function _getTextStyleForResult($res)
+    {
+        switch ($res) {
+            case Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_ERROR:
+                $color = Webgriffe_Golive_Helper_Output::COLOR_RED;
+                break;
+            case Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_WARNING:
+                $color = Webgriffe_Golive_Helper_Output::COLOR_YELLOW;
+                break;
+            case Webgriffe_Golive_Model_Checker_Abstract::SEVERITY_SKIP:
+                $color = Webgriffe_Golive_Helper_Output::COLOR_CYAN;
+                break;
+            default:
+                $color = Webgriffe_Golive_Helper_Output::COLOR_DEFAULT;
+                return $color;
+        }
+        return $color;
     }
 
 }
